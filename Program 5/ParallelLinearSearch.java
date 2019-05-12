321a49e5cc2a8957680e78015d2fe2034e3053ca
Marrah Delmont <s733605@sbuniv.edu>
2019-05-12 02:43:14 +0000

    Program 5

--- Program 5/ParallelLinearSearch.java
+++ Program 5/ParallelLinearSearch.java
@@ -1,14 +1,15 @@
 //Marrah Delmont
 //Program 5 - Parallel Linear Search
 //ADS - Spring 2019
-import java.util.Scanner; //used to take input from the user
-import java.util.Random; //used to fill array with random numbers
+//Write a linear search in Java using the Threads class to implement parallelism as discussed in class.
+import java.util.Scanner;
+import java.util.Random;
 
-class MyClass extends Thread
+class MyClass extends Thread//A thread is a thread of execution in a program. The Java Virtual Machine allows an application to have multiple threads of execution running concurrently.
 {
   int start;
   int end;
-  int searchNum; //key number
+  int searchNum;
   int [] array;
 
   MyClass(int start, int end, int search, int [] arr)
@@ -17,48 +18,46 @@ class MyClass extends Thread
     this.end = end;
     searchNum = search;
     array = arr;
-  }//end MyClass
-
-  public void run()//searcher
+  }
+  public void run()
   {
-    for(int j = start; j <= end; j++)//searches through array
-    {
-      if(array[j] == searchNum)//if key number is found
+    for(int j = start; j <= end; j++)//searches through the array
       {
-        System.out.println(searchNum + " was found at index " +j);
-      }//end if
-    }//end for 
-  }//end run
-}//end class MyClass
-
+        if(array[j] == searchNum)//if the key number is found
+        {
+          System.out.println(searchNum + " was found at index " +j);//then it will print where the key number was found
+        }
+      }
+  }
+}
 public class ParallelLinearSearch
 {
   public static void main(String args[])
   {
   int searchNum;
-  int arrayLength = 1000;
+  int arrayLength = 1000;//can be however big or small
   int[] array = new int[arrayLength];
   Scanner scan = new Scanner(System.in);
   Random rand = new Random();
   //Have your main method create a random array
   for(int i = 0; i < arrayLength; i++)
   {
-    array[i] = rand.nextInt(10) + 1;
+    array[i] = rand.nextInt(10) + 1;//fills array with random values
     System.out.print(array[i] + " "); //Print array to screen
-  }//end for
+  }
 
   System.out.println("What number are you searching for between 1 and 10? "); //Ask the user for a target value
   searchNum = scan.nextInt();
 
-  /*Look at different sections and take parameters
-  LS Lauren = new LS(array,0,499,target);  
-  LS Seth = new LS(array,500,999,target);
-  Seth.start();                             
-  Lauren.start();    
-  Have 2 threads, each searching a different half of the array for all matches in parallel, reporting the index locations of all matches to the screen.*/ 
+//Look at different sections and take parameters
+// LS Lauren = new LS(array,0,499,target);  
+// LS Seth = new LS(array,500,999,target);
+//Seth.start();                             
+//Lauren.start();    
+//Have 2 threads, each searching a different half of the array for all matches in parallel, reporting the index locations of all matches to the screen.
   MyClass a = new MyClass(0, 499, searchNum, array);
   MyClass b = new MyClass(500, 999, searchNum, array);
   a.start();
   b.start();
-  }//end main
-}//end class ParallelLinearSearch
+  }
+}
--- Program 6/ParallelCounterSort.java
+++ Program 6/ParallelCounterSort.java
@@ -1,29 +1,14 @@
-/*
-give the computer the 2 counter arrays rather than 2 sorted arrays
-    pass in singualr array tell threads which section of array to work with (0-49) then (50-99)
-    give back the totals, computer merges total into grand total, then replicates the data
-    subtotals are handed back by returning array, cannot be sent through the public void run though
-    you can make data array data memeber of the class. you can make get method to get the data array
-    add up subtotals count array inside run mehtod but have it located in the class object as a data memeber of the object.
-    get method will allow the main to reach into the object and get the array
-    get from both objects, add subcomponents, get grand total count array, the replicate data from that
-    (This way avoids the problem of shared memory problem)
-    does not print to a file 
-*/
 //Marrah Delmont
-//Program 6
+//Program 6 - Parallel Counter Sort
 //ADS - Spring 2019
+/* Write Program 6 in Java using the Threads class to implement parallelism as discussed in class.
+    Write a Counter Sort method that will Counter Sort any array of integers.*/
 import java.util.Random;//used to fill unsorted array with random values
-import java.lang.Math;//used for out of bounds
 public class ParallelCounterSort
 {
   public static void main(String [] args)
   {
     //make an array to be sorted
-    int counter = 0;
-    int min = 11;//set at 11 to go through and find smallest number in 0-10 number filled array[100]
-    int max = -1;//finds largest number from 0-10
-    int length;
     int [] array = new int[100];//holds the unsorted values
     int [] newArray = new int[100];//holds the sorted values
     int [] counterArray1 = new int[50];//will sort the first half - half the size of the array
@@ -34,24 +19,16 @@ public class ParallelCounterSort
     {
       array[i] = rand.nextInt(10) + 1;
       System.out.print(array[i] + " ");//prints unsorted array
-      if(array[i] > max)
-      {
-        max = array[i];//finds largest number
-      }//end if
-      if(array[i] < min)
-      {
-        min = array[i];//finds the smallest number
-      }//end if
     }
-    length = max - min;
     //create threads
-    MyClass a = new MyClass(0, 49, array, length, min, max, counterArray1);
-    MyClass b = new MyClass(50, 99, array, length, min, max, counterArray2);
+    MyClass a = new MyClass(0, 49, array, counterArray1);
+    MyClass b = new MyClass(50, 99, array, counterArray2);
     //start 
     a.start();
     b.start();
+    System.out.println("\nThe sorted array: ");
     //join 
-    try //if you do not do try/cath, an error message will display
+    try //if you do not do try/catch, an error message will display
     {
       a.join();
       b.join();
@@ -63,47 +40,35 @@ public class ParallelCounterSort
     for(int index = 0; index < counterArray1.length; index++)
     {
       int instances = counterArray1[index] + counterArray2[index];
-      counter = counter + instances;
-      for(int p = counter - instances; p < counter; p++)
+      for(int p = 0; p < instances; p++)
       {
-        newArray[p] = index + 1;
-        System.out.println(newArray[p]);//prints sorted values to the new array 
+        newArray[p] = index;
+        System.out.print(newArray[p]+ " ");//prints sorted values to the new array 
       }//end inner for
     }//end outer for
   }//end main
 }//end class ParallelCounterSort
 
-class MyClass extends Thread
+class MyClass extends Thread //A thread is a thread of execution in a program. The Java Virtual Machine allows an application to have multiple threads of execution running concurrently.
 {
-  int startIndex;
-  int endIndex;
+  int start;
+  int end;
   int array[];
   int counterArray[];
-  int length;
-  int min;
-  int max;
 
-  MyClass(int startIndex, int endIndex, int [] array, int length, int min, int max, int [] counterArray)
+  MyClass(int start, int end, int [] array, int [] counterArray)
   {
-    this.startIndex = startIndex;
-    this.endIndex = endIndex;
+    this.start = start;
+    this.end = end;
     this.array = array;
-    this.length = length;
-    this.min = min;
-    this.max = max;
     this.counterArray = counterArray;
-  }//constructor
-  
+  }
   public void run()
   {
-      int [] newArray = new int[array.length];//new array is the size of the unsorted array
-      int counter = 0;
-    //go through data array and counter sort the dooblie doo where index 0 represents min, 1 represents min + 1, all the way to n being max
-      for(int j = startIndex; j < endIndex; j++)
+      for(int j = start; j < end; j++)
       {
         int value = array[j];
-        counterArray[value - min ] = counterArray[value - min] + 1;
-        System.out.println("value: " + value + "   min: " + min);
+        counterArray[value] = counterArray[value] + 1;
       }//end for
   }//end run
 }//end class MyClass
